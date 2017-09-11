f5e38f0bffaec8abfb311f82553b55042529eb7f
Isaac Vallee <isaacvallee@gmail.com>
2017-09-11 23:09:19 +0000

    linted

--- request.js
+++ request.js
@@ -1,11 +1,11 @@
 const request = require('request');
 const fs = require('fs');
 
-request.get('https://sytantris.github.io/http-examples/future.jpg')               // Note 1
-       .on('error', function (err) {                                   // Note 2
-         throw err;
-       })
-       .on('response', function (response) {                           // Note 3
-         console.log('Response Message: ', response.statusMessage + "\n", response.headers["content-type"]);
-       })
-       .pipe(fs.createWriteStream('./future.jpg'));
+request.get('https://sytantris.github.io/http-examples/future.jpg')
+  .on('error', function (err) {
+    throw err;
+  })
+  .on('response', function (response) {
+    console.log('Response Message: ', response.statusMessage + "\n", response.headers["content-type"]);
+  })
+  .pipe(fs.createWriteStream('./future.jpg'));
