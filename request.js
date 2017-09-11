16368277913723ea965c6e32d21946de7ea1f044
Isaac Vallee <isaacvallee@gmail.com>
2017-09-11 23:23:15 +0000

    added console logs

--- request.js
+++ request.js
@@ -1,11 +1,14 @@
 const request = require('request');
 const fs = require('fs');
 
+console.log('Downloading image...');
+
 request.get('https://sytantris.github.io/http-examples/future.jpg')
   .on('error', function (err) {
     throw err;
   })
   .on('response', function (response) {
     console.log('Response Message: ', response.statusMessage + "\n", response.headers["content-type"]);
+    console.log('Download Complete');
   })
   .pipe(fs.createWriteStream('./future.jpg'));
