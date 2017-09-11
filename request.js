1f74a1f08d50efdceaab5c69105f5f5fc81dfe3c
Isaac Vallee <isaacvallee@gmail.com>
2017-09-11 23:08:16 +0000

    completed exercise

--- request.js
+++ request.js
@@ -1,11 +1,11 @@
 const request = require('request');
 const fs = require('fs');
 
-request.get('https://sytantris.github.io/http-examples')               // Note 1
+request.get('https://sytantris.github.io/http-examples/future.jpg')               // Note 1
        .on('error', function (err) {                                   // Note 2
          throw err;
        })
        .on('response', function (response) {                           // Note 3
-         console.log('Response Status Code: ', response.statusCode);
+         console.log('Response Message: ', response.statusMessage + "\n", response.headers["content-type"]);
        })
-       .pipe(fs.createWriteStream('./downloaded.html'));
+       .pipe(fs.createWriteStream('./future.jpg'));
