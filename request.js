6fbc5029785910b868d3338774bab56288345adb
Isaac Vallee <isaacvallee@gmail.com>
2017-09-11 22:04:11 +0000

    initial commit

---
+++ request.js
@@ -0,0 +1,11 @@
+const request = require('request');
+const fs = require('fs');
+
+request.get('https://sytantris.github.io/http-examples')               // Note 1
+       .on('error', function (err) {                                   // Note 2
+         throw err;
+       })
+       .on('response', function (response) {                           // Note 3
+         console.log('Response Status Code: ', response.statusCode);
+       })
+       .pipe(fs.createWriteStream('./downloaded.html'));
